f617d6bdcc1e4978301e97786712dde64d410c40
taimooralitalpur <talpurtaimoorali@gmail.com>
2024-05-25 18:51:07 +0500

    Age Calculator

---
+++ app.js
@@ -0,0 +1,10 @@
+function calculateAge() {
+    var dob = new Date(document.getElementById("dob").value);
+    var today = new Date();
+    var age = today.getFullYear() - dob.getFullYear();
+    var m = today.getMonth() - dob.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
+      age--;
+    }
+    document.getElementById("age").innerHTML = "Your age is " + age;
+  }
